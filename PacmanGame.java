d29d8a39d9c6e3da7c09f2c352319fdeabeb3249
Алекс Ш <slayer1649@gmail.com>
2023-08-17 18:07:30 +0300

    update

--- PacmanGame.java
+++ PacmanGame.java
@@ -45,12 +45,12 @@ public class PacmanGame extends JPanel implements ActionListener {
     }
 
     private void loadImages() {
-        down = new ImageIcon("/src/images/down.gif").getImage();
-        up = new ImageIcon("/src/images/up.gif").getImage();
-        left = new ImageIcon("/src/images/left.gif").getImage();
-        right = new ImageIcon("/src/images/right.gif").getImage();
-        ghost = new ImageIcon("/src/images/ghost.gif").getImage();
-        heart = new ImageIcon("/src/images/heart.png").getImage();
+        down = new ImageIcon("/images/down.gif").getImage();
+        up = new ImageIcon("/images/up.gif").getImage();
+        left = new ImageIcon("/images/left.gif").getImage();
+        right = new ImageIcon("/images/right.gif").getImage();
+        ghost = new ImageIcon("/images/ghost.gif").getImage();
+        heart = new ImageIcon("/images/heart.png").getImage();
     }
 
     private void initVariables() {
