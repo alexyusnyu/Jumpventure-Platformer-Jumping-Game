d318c4b2158eb6548f00f31e8c95e22a4bcfd16e
Алекс Ш <slayer1649@gmail.com>
2023-08-18 11:00:44 +0300

    player texture added

--- player.py
+++ player.py
@@ -2,47 +2,45 @@ import pygame
 
 class Player:
     def __init__(self, screen_width, screen_height):
-        self.image = pygame.Surface((30, 30))
-        self.image.fill((255, 0, 0))
+        self.image = pygame.image.load("player_texture.png").convert_alpha()  # Load player texture
+        self.image = pygame.transform.scale(self.image, (50, 50))  # Adjust player size
         self.rect = self.image.get_rect()
         self.rect.center = (screen_width // 2, 0)
-
-        self.velocity_y = 0
-        self.camera_y = 0
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-
+        self.velocity = 0
+        self.gravity = 1
         self.is_jumping = False
-        self.jump_power = -12
+        self.jump_count = 0
+        self.jump_power = -15
+        self.camera_y = 0
+        self.screen_height = screen_height  # Store screen height
+
+    def jump(self):
+        if not self.is_jumping and self.jump_count < 2:
+            self.velocity = self.jump_power
+            self.is_jumping = True
+            self.jump_count += 1
 
     def update(self, keys, platforms):
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= 5
-            self.rect.x = max(self.rect.x, 0)  # Prevent from going off the left edge
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 5
-            self.rect.x = min(self.rect.x, self.screen_width - self.rect.width)  # Prevent from going off the right edge
+        self.velocity += self.gravity
+        self.rect.y += self.velocity
 
-        if keys[pygame.K_SPACE] and not self.is_jumping:
-            self.velocity_y = self.jump_power
-            self.is_jumping = True
-
-        self.velocity_y += 0.5
-        self.rect.y += self.velocity_y
-
-        # Update camera position
-        self.camera_y = max(self.rect.y - self.screen_height // 2, 0)
-
-        self.check_collision(platforms)
-
-    def check_collision(self, platforms):
-        for platform in platforms:
-            if self.rect.colliderect(platform.rect) and self.velocity_y > 0 and self.rect.centery < platform.rect.top:
-                self.rect.bottom = platform.rect.top
-                self.velocity_y = 0
-                self.is_jumping = False
-
-        if self.rect.bottom > self.screen_height:
+        if self.rect.bottom >= self.screen_height:  # Use screen_height here
             self.rect.bottom = self.screen_height
-            self.velocity_y = 0
+            self.velocity = 0
             self.is_jumping = False
+            self.jump_count = 0
+
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect) and self.velocity > 0:
+                self.rect.bottom = platform.rect.top
+                self.velocity = 0
+                self.is_jumping = False
+                self.jump_count = 0
+
+    def move_camera(self):
+        if self.rect.top <= self.camera_y + 200:
+            self.camera_y -= 5  # Adjust camera movement speed
+
+    def stop_camera(self):
+        if self.rect.top >= self.camera_y + 400:
+            self.camera_y += 5  # Adjust camera movement speed
